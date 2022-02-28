eae0eb1a46477d67a9a10054537aa67152be872c
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:54:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loomine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inimene naine = new Inimene("Kask");
+            Console.WriteLine("Kui vana sa oled");
+            naine.Vanus = int.Parse(Console.ReadLine());
+            Console.WriteLine("Missugune on sinu palk?");
+            naine.Palk = int.Parse(Console.ReadLine());
+            naine.tervitamine();
+
+            Inimene mees = new Inimene();
+            mees.Perenimi = "Tamm";
+            mees.Vanus = 69;
+            mees.Palk = 14000;
+            Console.WriteLine(mees.Perenimi+" on "+mees.Vanus+" aastane mees", "palk on "+mees.palk);
+            mees.tervitamine();
+            Inimene[] inimesed = new Inimene[5];
+            for (int i = 0; i < 5; i++)
+            {
+                inimesed[1] = new Inimene();
+                Console.WriteLine("Mis on sinu nimi");
+                inimesed[i].Perenimi = Console.ReadLine();
+                Console.WriteLine("Kui vana sa oled? Kuradi pidur...");
+                inimesed[i].Vanus = int.Parse(Console.ReadLine());
+            }
+            foreach (Inimene inimene in inimesed)
+            {
+                inimene.tervitamine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ inimene.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loomine
+{
+    class Inimene
+    {
+        string eesnimi;
+        string sugu;
+        string perenimi;
+        string staatus;
+        int vanus;
+        int palk;
+        public Inimene() { }
+        public Inimene(string Perenimi)
+        {
+            perenimi = Perenimi;
+        }
+        public Inimene(string eesnimi, string perenimi)
+        {
+            Eesnimi = eesnimi;
+            Perenimi = perenimi;
+        }
+        public string Sugu
+        {
+            set { if (sugu == null) sugu = value; }
+            get { return sugu; }
+        }
+        public string Eesnimi
+        {
+            set { if (eesnimi == null) eesnimi = value; }
+            get { return eesnimi; }
+        }
+        public string Perenimi
+        {
+            set { if (perenimi == null) perenimi = value; }
+            get { return perenimi; }
+        }
+        public int Vanus
+        {
+            set
+            {
+                vanus = value;
+                if (vanus < 7)
+                {
+                    staatus = "laps";
+                }
+                else if (vanus < 17)
+                {
+                    staatus="koolilaps";
+                }
+                else if (vanus < 24)
+                {
+                    staatus = "ülikoolilaps";
+                }
+                else
+                {
+                    staatus = "tööline";
+                }
+
+            }
+            get { return vanus; }
+        }
+        public string Staatus
+        {
+            get { return staatus; }
+        }
+        public int Palk
+        {
+            set
+            {
+                palk = value;
+                if (palk < 450)
+                {
+                    staatus = "õpilane";
+                }
+                else if (palk < 800)
+                {
+                    staatus = "Lihtsalt inimene";
+                }
+                else if (palk < 1200)
+                {
+                    staatus = "rikas";
+                }
+                else
+                {
+                    staatus = "kodutu";
+                }
+            }
+        }
+
+
+        public void tervitamine()
+        {
+            Console.WriteLine("tere! minu perenimi on "+perenimi);
+            Console.WriteLine("Ma olen {0} aastat vana, olen {1}",vanus,staatus,sugu,palk);
+        }
+    }
+}
